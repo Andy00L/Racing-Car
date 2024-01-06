32ce09d9ab9f06b5a06ac07a40dafb60a4303157
Andy <89641810+Andy00L@users.noreply.github.com>
2024-01-06 01:53:28 -0500

    Update main.py
    Some finished code, need to fix the turning computer car when it wins

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import pygame
 import time
 import math
 
-from outils import taille_image, blit_centre_rotation
+from outils import taille_image, blit_centre_rotation, blit_text_center
+
+pygame.font.init()
 
 HERBE = taille_image(pygame.image.load("img/grass.jpg"), 2.5)
 PISTE = taille_image(pygame.image.load("img/track.png"), 0.9)
@@ -21,12 +23,43 @@ LARGEUR, HAUTEUR = PISTE.get_width(), PISTE.get_height()
 
 FENETRE = pygame.display.set_mode((LARGEUR, HAUTEUR))
 
+MAIN_FONT = pygame.font.SysFont("comicsans", 44)
+
 PATH = [(175, 119), (110, 70), (56, 133), (70, 481), (318, 731), (404, 680), (418, 521), (507, 475), (600, 551), (613, 715), (736, 713),
         (734, 399), (611, 357), (409, 343), (433, 257), (697, 258), (738, 123), (581, 71), (303, 78), (275, 377), (176, 388), (178, 260)]
 
 pygame.display.set_caption(" jeu de course!")
 
 FPS = 60
+
+class InfoJeu:
+    NIVEAUX = 10
+    
+    def __init__(self, niveau=1):
+        self.niveau = niveau
+        self.depart = False
+        self.temp_debut_niveau = 0
+    
+    def next_niveau(self):
+        self.niveau += 1
+        self.depart = False
+        
+    def reset(self):
+        self.niveau = 1
+        self.depart = False
+        self.temp_debut_niveau = 0
+        
+    def fin_jeu(self):
+        return self.niveau > self.NIVEAUX
+    
+    def debut_niveau(self):
+        self.depart = True
+        self.temp_debut_niveau = time.time()
+    
+    def avoir_temp_niveau(self):
+        if not self.depart:
+            return 0 
+        return round(time.time() - self.temp_debut_niveau) 
 
 class AbstractCar:
     def __init__(self, vel_max , vel_rotation):
@@ -138,13 +171,31 @@ class voitureOrdi(AbstractCar):
         self.calculer_angle()
         self.update_path_point()
         super().bouger()
+    
+    def next_niveau(self, niveau):
+        self.reset()
+        self.vel = self.vel_max + (niveau - 1) * 0.2
+        self.position_present = 0
         
 # La fonction 'dessiner' prend deux parametre, 'fenetre' qui est une la taile de la fenettre du jeu
 # et ' images' qui est une liste de listes contenant une image et sa position. Elle permet de les dessiner
 # le terrain et ces objets.
-def dessiner(fenetre, images, voiture_joueur, voiture_ordi):
+def dessiner(fenetre, images, voiture_joueur, voiture_ordi, info_jeu):
     for img, position in images:
         fenetre.blit(img, position)
+        
+    text_niveau = MAIN_FONT.render(
+        f"Niveau{info_jeu.niveau}", 1, (255, 255, 255))
+    fenetre.blit(text_niveau, (10, HAUTEUR - text_niveau.get_height() - 70))
+    
+    text_temp = MAIN_FONT.render(
+        f"Temp: {info_jeu.avoir_temp_niveau()}s", 1, (255, 255, 255))
+    fenetre.blit(text_temp, (10, HAUTEUR - text_temp.get_height() - 40))
+    
+    text_vel = MAIN_FONT.render(
+        f"Vel: {round(voiture_joueur.vel, 1)} px/s", 1, (255, 255, 255))
+    fenetre.blit(text_vel, (10, HAUTEUR - text_vel.get_height() - 10))
+    
     voiture_joueur.dessiner(fenetre)
     voiture_ordi.dessiner(fenetre)
     pygame.display.update()
@@ -167,16 +218,19 @@ def bouger_joueur(voiture_joueur):
     if not bougeer:
         voiture_joueur.reduire_vitesse()   
         
-def gerer_colision(voiture_joueur, voiture_ordi):
+def gerer_colision(voiture_joueur, voiture_ordi, info_jeu):
     if voiture_joueur.collision(BORDUR_PISTE_MASK) != None:
         voiture_joueur.rebondir()
     
     
     ordi_arriver_position_collision = voiture_ordi.collision(ARRIVER_MASK, *POSITION_ARRIVER)
     if ordi_arriver_position_collision != None:
+        blit_text_center(FENETRE, MAIN_FONT, "Tu as perdu!!!")
+        pygame.display.update()
+        pygame.time.wait(3000)
+        info_jeu.reset()
         voiture_joueur.reset()
         voiture_ordi.reset()
-        print(" Computer gagner!")
     
     
     joueur_arriver_position_collision = voiture_joueur.collision(ARRIVER_MASK, *POSITION_ARRIVER)
@@ -184,23 +238,34 @@ def gerer_colision(voiture_joueur, voiture_ordi):
         if joueur_arriver_position_collision[1] == 0:
             voiture_joueur.rebondir()
         else: 
+            info_jeu.next_niveau()
             voiture_joueur.reset()
-            voiture_ordi.reset()
+            voiture_ordi.next_niveau(info_jeu.niveau)
 
 run = True
 # Permet de fixer la vitesse du jeu.
 horloge = pygame.time.Clock()
 images = [(HERBE, (0,0)), (PISTE, (0,0)), (ARRIVER, POSITION_ARRIVER), (BORDUR_PISTE, (0,0))]
 voiture_joueur = VoitureJoueur(4, 4)
-voiture_ordi = voitureOrdi(4, 4, PATH)
-
+voiture_ordi = voitureOrdi(2, 4, PATH)
+info_jeu = InfoJeu()
 while run:
     # empeche le heu de tourner a plus de 60 fps.
     horloge.tick(FPS)
     
     # Permet de dessiner le terrain
-    dessiner(FENETRE, images, voiture_joueur, voiture_ordi)
+    dessiner(FENETRE, images, voiture_joueur, voiture_ordi, info_jeu)
     
+    while not info_jeu.depart:
+        blit_text_center(FENETRE, MAIN_FONT,f"Appuyer sur une touche pour jouer {info_jeu.niveau}!")
+        pygame.display.update()
+        for evenement in pygame.event.get():
+            if evenement.type == pygame.QUIT:
+                pygame.quit()
+                break
+            if evenement.type == pygame.KEYDOWN:
+                info_jeu.debut_niveau()
+        
     for evenement in pygame.event.get():
         if evenement.type == pygame.QUIT:
             run = False
@@ -210,8 +275,13 @@ while run:
     bouger_joueur(voiture_joueur)
     voiture_ordi.bouger()
     
-    gerer_colision(voiture_joueur , voiture_ordi)
+    gerer_colision(voiture_joueur , voiture_ordi, info_jeu)
     
+    if info_jeu.fin_jeu():
+        blit_text_center(FENETRE, MAIN_FONT, "Tu as gagner !!!")
+        pygame.time.wait(3000)
+        info_jeu.reset()
+        voiture_joueur.reset()
+        voiture_ordi.reset()
             
-print(voiture_ordi.path)
 pygame.quit()
